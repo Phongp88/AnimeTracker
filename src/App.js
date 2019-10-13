c26d4f573605b20300bfe47d4973985d0cddfd8b
phong <phong_pham88@hotmail.com>
2019-10-12 22:57:18 -0600

    added card component and search bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/card';
+import Searchbar from './components/searchbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Searchbar />
+        <Card/>
+      {/* <header className="App-header">
+        <form action="/quotes" method="POST">
+        <input type="text" placeholder="name" name="name"></input>
+        <input type="text" placeholder="quote" name="quote"></input>
+        <button type="submit">Submit</button>
+        </form>
+      </header> */}
     </div>
   );
 }
